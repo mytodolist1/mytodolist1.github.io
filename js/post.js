428f62be42c0ea39e980d9b306fee02063691d61
mytodolist1 <dirgafebrian45@gmail.com>
2023-10-21 14:46:23 +0700

    update

--- js/post.js
+++ js/post.js
@@ -1,6 +1,6 @@
 // import { postWithToken } from "https://jscroot.github.io/api/croot.js";
 // import {setInner,getValue} from "https://jscroot.github.io/element/croot.js";
 
-import PostSignUp from "./postfunction.js";
+import PostSignUp from "./postfunc.js";
 
 window.PostSignUp = PostSignUp;

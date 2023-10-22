8e24b462525cb494a956cd5ae68a43b7ab668a9e
mytodolist1 <dirgafebrian45@gmail.com>
2023-10-22 15:53:52 +0700

    update

--- js/postfunc.js
+++ js/postfunc.js
@@ -3,7 +3,7 @@ import {setInner,getValue} from "https://jscroot.github.io/element/croot.js";
 import {setCookieWithExpireHour} from "https://jscroot.github.io/cookie/croot.js";
 
 export default function PostSignUp() {
-    let target_url = "https://asia-southeast2-mytodolist-402507.cloudfunctions.net/mytodolist";
+    let target_url = "https://asia-southeast2-mytodolist-402507.cloudfunctions.net/mytodolist-post";
     let tokenkey = "token";
     let tokenvalue = "d4f1c80e75682f5cc33895fb3ccfe6e5165fea6df42d463eb7144e34db2d3ef0";
     let datainjson = {
@@ -20,7 +20,7 @@ function responseData(result) {
     if (result.message == "Selamat Datang") {
         setCookieWithExpireHour("token", result.token, 2);
         alert("Berhasil Masuk " + result.message)
-        window.location.href = "dashboard.html";
+        window.location.href = "../dashboard.html";
     } else {
         alert("Gagal Masuk " + "password atau username salah")
         console.log(result.message);

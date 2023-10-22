674c1278bcd11946b155a23e2f0e2f1ce6427e6e
mytodolist1 <dirgafebrian45@gmail.com>
2023-10-22 17:20:06 +0700

    update

--- js/postfunc.js
+++ js/postfunc.js
@@ -15,47 +15,14 @@ export default function PostSignUp(){
 
 }
 function responseData(result) {
-    setInner("pesan", result.message);
-
-    console.log(result.status);
     alert(result.message);
     
     if (result.message === "Selamat Datang") {
-        // Jika pesan adalah "Selamat Datang", arahkan ke halaman dashboard.
         setCookieWithExpireHour("token", result.token, 2);
-        window.location.href = "dashboard.html"; // Gantilah "error.html" dengan halaman error yang sesuai.
+        window.location.href = "dashboard.html"; 
     } else if (result.message === "Password Salah") {
-        // Jika pesan kesalahan adalah "Password salah", arahkan ke halaman error.
         window.location.href = "login.html";
     } else {
-        // Penanganan lainnya (pesan kesalahan lainnya)
-        window.location.href = "404.html";
+        return false;
     }
 }
-
-
-// export default function PostSignUp() {
-//     let target_url = "https://asia-southeast2-mytodolist-402507.cloudfunctions.net/mytodolist-post";
-//     let tokenkey = "token";
-//     let tokenvalue = "d4f1c80e75682f5cc33895fb3ccfe6e5165fea6df42d463eb7144e34db2d3ef0";
-//     let datainjson = {
-//         "username": getValue("username"),
-//         "password": getValue("password")
-//     }
-
-//     postWithToken(target_url,tokenkey,tokenvalue,datainjson,responseData);
-// }
-
-// function responseData(result) {
-
-//     setInner("pesan", result.message);
-//     if (result.message == "Selamat Datang") {
-//         setCookieWithExpireHour("token", result.token, 2);
-//         alert("Berhasil Masuk " + result.message)
-//         window.location.href = "../dashboard.html";
-//     } else {
-//         alert("Gagal Masuk " + "password atau username salah")
-//         console.log(result.message);
-//     }
-// }
-

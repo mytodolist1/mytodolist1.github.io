6e4d662e6dd40d48cdbf2fd7ddcf4f019d75008a
mytodolist1 <dirgafebrian45@gmail.com>
2023-10-22 18:48:23 +0700

    update

--- js/postfunc.js
+++ js/postfunc.js
@@ -19,7 +19,7 @@ function responseData(result) {
     
     if (result.message === "Selamat Datang") {
         setCookieWithExpireHour("token", result.token, 2);
-        window.location.href = "dashboard.html"; 
+        window.location.href = "index.html"; 
     } else if (result.message === "Password Salah") {
         window.location.href = "login.html";
     } else {

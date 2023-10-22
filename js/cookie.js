b47d860abaa6af6242fc2633f79202ca6fc2a8df
mytodolist1 <dirgafebrian45@gmail.com>
2023-10-22 16:18:18 +0700

    update

--- js/cookie.js
+++ js/cookie.js
@@ -2,8 +2,19 @@ import { deleteCookie } from "https://jscroot.github.io/cookie/croot.js";
 // import { setInner } from "https://jscroot.github.io/element/croot.js";
 
 function logout() {
-    deleteCookie("token");
-    window.location.href = "login.html";
+    var result = confirm("Are you sure you want to logout?");
+    if (result) {
+        deleteCookie("token");
+        document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+
+        const currentPath = window.location.pathname.split("/").pop();
+
+        if (currentPath === "index.html") {
+            window.location.href = "login.html";
+        } else {
+            window.location.href = "login.html";
+        }
+    }
 }
 
 document.getElementById("logoutButton").addEventListener("click", logout);
--- js/postfunc.js
+++ js/postfunc.js
@@ -2,28 +2,27 @@ import { postWithToken } from "https://jscroot.github.io/api/croot.js";
 import {setInner,getValue} from "https://jscroot.github.io/element/croot.js";
 import {setCookieWithExpireHour} from "https://jscroot.github.io/cookie/croot.js";
 
-import { postWithToken } from "https://jscroot.github.io/api/croot.js";
-import {setInner,getValue} from "https://jscroot.github.io/element/croot.js";
-import {setCookieWithExpireHour} from "https://jscroot.github.io/cookie/croot.js";
-
 export default function PostSignUp(){
     let target_url = "https://asia-southeast2-mytodolist-402507.cloudfunctions.net/mytodolist-post";
     let tokenkey = "token";
     let tokenvalue = "d4f1c80e75682f5cc33895fb3ccfe6e5165fea6df42d463eb7144e34db2d3ef0"; 
     let datainjson = {
         "username": getValue("username"),
-        "password": getValue("password")
-    }
+        "password": getValue("password"),
+    };
 
     postWithToken(target_url,tokenkey,tokenvalue,datainjson,responseData);
 
 }
 function responseData(result) {
     setInner("pesan", result.message);
-    setCookieWithExpireHour("token", result.token, 2);
+
+    console.log(result.status);
+    alert(result.message);
     
     if (result.message === "Selamat Datang") {
         // Jika pesan adalah "Selamat Datang", arahkan ke halaman dashboard.
+        setCookieWithExpireHour("token", result.token, 2);
         window.location.href = "dashboard.html"; // Gantilah "error.html" dengan halaman error yang sesuai.
     } else if (result.message === "Password Salah") {
         // Jika pesan kesalahan adalah "Password salah", arahkan ke halaman error.

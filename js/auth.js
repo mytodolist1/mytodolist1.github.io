ee3d32b4df0fb9be4b039b3128e7bf19822845ae
mytodolist1 <dirgafebrian45@gmail.com>
2023-10-26 19:13:47 +0700

    add auth.js

--- js/auth.js
+++ js/auth.js
@@ -2,9 +2,12 @@
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 
 export function checkTokenAndRedirect() {
-    const token = getCookie("token");
-    if (!token) {
+    const tokens = "token";
+    if (!getCookie(tokens)) {
         // Token tidak ditemukan dalam cookie, arahkan ke halaman login
         window.location.href = "login.html";
+    } else {
+        // Token ditemukan dalam cookie, arahkan ke halaman dashboard
+        window.location.href = "index.html";
     }
 }

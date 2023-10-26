542d346cb75541f3c911f85e7712a0f976c1ce95
mytodolist1 <dirgafebrian45@gmail.com>
2023-10-26 20:00:12 +0700

    update

--- js/auth.js
+++ js/auth.js
@@ -3,10 +3,10 @@ import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 function checkTokenAndRedirect() {
     const tokens = "token";
     const tokenValue = getCookie(tokens);
-    console.log("Token Value:", tokenValue);
+    // console.log("Token Value:", tokenValue);
 
     if (!tokenValue) {
-        console.log("Token not found, redirecting to login.");
+        // console.log("Token not found, redirecting to login.");
         window.location.href = "login.html";
     }
 }

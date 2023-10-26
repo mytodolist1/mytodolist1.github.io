1046b5151e4d9de1ad9067f4b410b5ab688e270a
mytodolist1 <dirgafebrian45@gmail.com>
2023-10-26 15:13:25 +0700

    feature(authentication): Menambahkan sistem otentikasi pengguna

---
+++ js/auth.js
@@ -0,0 +1,17 @@
+import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
+
+// Fungsi untuk memeriksa sesi dan mengarahkan pengguna
+function checkSessionAndRedirect() {
+  const token = getCookie("token");
+
+  if (token) {
+    // Token ditemukan, pengguna tetap di dashboard
+    window.location.href = "dashboard.html"; // Ganti "dashboard.html" dengan URL halaman dashboard yang sesuai
+  } else {
+    // Token tidak ditemukan, arahkan ke halaman login
+    window.location.href = "login.html"; // Ganti "login.html" dengan URL halaman login yang sesuai
+  }
+}
+
+// Panggil fungsi ini saat halaman dimuat
+window.onload = checkSessionAndRedirect;
--- js/postfunc.js
+++ js/postfunc.js
@@ -1,6 +1,6 @@
 import { postWithToken } from "https://jscroot.github.io/api/croot.js";
-import {setInner,getValue} from "https://jscroot.github.io/element/croot.js";
-import {setCookieWithExpireHour} from "https://jscroot.github.io/cookie/croot.js";
+import { getValue } from "https://jscroot.github.io/element/croot.js";
+import { setCookieWithExpireHour } from "https://jscroot.github.io/cookie/croot.js";
 
 export default function PostSignUp(){
     let target_url = "https://asia-southeast2-mytodolist-402507.cloudfunctions.net/mytodolist-post";

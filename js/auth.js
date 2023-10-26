96a106d0a923c388e439e821fd37135ce74877eb
mytodolist1 <dirgafebrian45@gmail.com>
2023-10-26 19:50:54 +0700

    fix auth

--- js/auth.js
+++ js/auth.js
@@ -1,13 +1,13 @@
-// auth.js
-import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
-
 function checkTokenAndRedirect() {
     const tokens = "token";
-    if (!getCookie(tokens)) {
-        // Token tidak ditemukan dalam cookie, arahkan ke halaman login
+    const tokenValue = getCookie(tokens);
+    console.log("Token Value:", tokenValue);
+
+    if (!tokenValue) {
+        console.log("Token not found, redirecting to login.");
         window.location.href = "login.html";
     } else {
-        // Token ditemukan dalam cookie, arahkan ke halaman dashboard
+        console.log("Token found, redirecting to dashboard.");
         window.location.href = "index.html";
     }
 }

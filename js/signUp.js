9daed60d6febd77f46faf18c5d333f84549e9529
NaufalDekha002 <dekhawidananaufal@gmail.com>
2023-11-13 14:06:04 +0700

    create function register

---
+++ js/signUp.js
@@ -0,0 +1,24 @@
+import { post } from "./postfunc.js";
+import { getValue } from "https://jscroot.github.io/element/croot.js";
+
+const PostSignUp = () => {
+    const target_url = "https://asia-southeast2-mytodolist-402507.cloudfunctions.net/mytodolist-post"; //Note, waktu gwa coba dirumah masih kagak bisa
+    const datainjson = {
+        username: getValue("username"),
+        password: getValue("password")
+    };
+    console.log(datainjson);
+    post(target_url, datainjson, responseData);
+};
+
+const responseData = (result) => {
+    console.log(result);
+    if (result.status === true) {
+        alert(`Berhasil Masuk ${result.message}`);
+        window.location.href = "login.html";
+    } else {
+        alert(`Gagal Signup, ` + result.message );
+    }
+};
+
+window.PostSignUp = PostSignUp;

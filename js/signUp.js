474599d8c601c6ec12e795acd9e44d651832433c
dirgafebrian <dirgafebrian45@gmail.com>
2023-11-14 11:56:21 +0700

    update signup

--- js/signUp.js
+++ js/signUp.js
@@ -1,24 +1,21 @@
-import { post } from "./postfunc.js";
+import { postWithBearer } from "https://jscroot.github.io/api/croot.js";
 import { getValue } from "https://jscroot.github.io/element/croot.js";
 
-const PostSignUp = () => {
-    const target_url = "https://asia-southeast2-mytodolist-402507.cloudfunctions.net/mytodolist-post"; //Note, waktu gwa coba dirumah masih kagak bisa
-    const datainjson = {
-        username: getValue("username"),
-        password: getValue("password")
+function Register(){
+    let target_url = "https://asia-southeast2-mytodolist-402507.cloudfunctions.net/mytodolist-register";
+    let token = "token";
+
+    let data = {
+        "email": getValue("email"),
+        "username": getValue("username"),
+        "password": getValue("password"),
     };
-    console.log(datainjson);
-    post(target_url, datainjson, responseData);
-};
+    postWithBearer(target_url, token, data, responseData);
+}
 
-const responseData = (result) => {
-    console.log(result);
-    if (result.status === true) {
-        alert(`Berhasil Masuk ${result.message}`);
-        window.location.href = "login.html";
-    } else {
-        alert(`Gagal Signup, ` + result.message );
-    }
-};
+function responseData(result) {
+    alert(result.message + "\nRegistrasi Berhasil")
+    window.location.href= "login.html"
+}
 
-window.PostSignUp = PostSignUp;
+document.getElementById("button1").addEventListener("click", Register);

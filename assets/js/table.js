216ec2e4b8c981a7bf3eda66419b9373b6ef042d
dirgafebrian <dirgafebrian45@gmail.com>
2023-12-04 23:11:07 +0700

    update

--- assets/js/table.js
+++ assets/js/table.js
@@ -11,10 +11,10 @@ export const formTodolist = `
 <td data-label="Date">#DEADLINE#</td>
 <td class="is-actions-cell">
 <div class="buttons is-right">
-  <a href="edit.html?_id=#IDEDIT#" class="button is-primary jb-modal" data-todo-id="#IDHAPUS#" data-target="edit-modal" type="button">
+  <a href="edit.html?_id=#IDEDIT#" class="button is-dark jb-modal" data-todo-id="#IDHAPUS#" data-target="edit-modal" type="button">
     <span class="icon"><i class="mdi mdi-eye-circle"></i></span>
   </a>
-  <button class="button is-danger" type="button" onclick="deleteTodo('#DELETE#')">
+  <button class="button is-dark is-outlined" type="button" onclick="deleteTodo('#DELETE#')">
     <span class="icon"><i class="mdi mdi-delete-circle"></i></span>
   </button>
 </div>

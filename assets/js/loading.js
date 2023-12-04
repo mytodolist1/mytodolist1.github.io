8ee8d3d17503b99dc1cec918bc1183d2617f53df
dirgafebrian <dirgafebrian45@gmail.com>
2023-12-04 23:37:50 +0700

    add loading

---
+++ assets/js/loading.js
@@ -0,0 +1,8 @@
+  // Menghilangkan overlay saat halaman selesai dimuat
+  document.onreadystatechange = function () {
+    if (document.readyState === 'complete') {
+      setTimeout(function () {
+        document.getElementById('loader-wrapper').style.display = 'none';
+      }, 2000); // Sesuaikan timeout dengan durasi animasi CSS
+    }
+  };

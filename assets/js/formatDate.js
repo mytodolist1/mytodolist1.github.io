5cd624881b7a12096da23b525e64f78f99e46b63
dirgafebrian <dirgafebrian45@gmail.com>
2023-12-06 16:15:39 +0700

    update

--- assets/js/formatDate.js
+++ assets/js/formatDate.js
@@ -1,12 +1,13 @@
-export const formatDate = () => {
-    const deadlineInput = document.getElementById('deadline');
-    const deadlineDate = new Date(deadlineInput.value);
-    const formattedDeadline = deadlineDate.toLocaleDateString('id-ID');
-    deadlineInput.value = formattedDeadline;
+export const formatDate = (inputValue) => {
+    let date;
 
-    deadlineInput.addEventListener('change', () => {
-        const deadlineDate = new Date(deadlineInput.value);
-        const formattedDeadline = deadlineDate.toLocaleDateString('id-ID');
-        deadlineInput.value = formattedDeadline;
-    });
-}
+    if (inputValue) {
+        date = new Date(inputValue);
+    } else {
+        date = new Date();
+    }
+
+    return `${date.getDate().toString().padStart(2, '0')}/
+    ${(date.getMonth() + 1).toString().padStart(2, '0')}/
+    ${date.getFullYear()}`;
+};

1a2e55710206d28c96a27ea5a51bf7b3356b5ff7
dirgafebrian <dirgafebrian45@gmail.com>
2023-12-07 15:23:47 +0700

    update date time

--- assets/js/postTodo.js
+++ assets/js/postTodo.js
@@ -1,30 +1,21 @@
-// script.js
-
 import { postWithToken } from "https://jscroot.github.io/api/croot.js";
 import { getValue } from "https://jscroot.github.io/element/croot.js";
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
-import { formatDate } from "./formatDate";
 
 const insertTodo = () => {
     const target_url = "https://asia-southeast2-mytodolist-402507.cloudfunctions.net/mytodolist-insertTodo";
     const tokenkey = "Authorization";
     const tokenvalue = getCookie("Authorization");
 
-    // Ambil nilai dari input tanggal
     const deadlineInput = document.getElementById('deadline');
-
-    // Set nilai awal ke tanggal saat ini jika kosong
-    if (!deadlineInput.value) {
-        deadlineInput.value = formatDate();
-    }
-
-    // Gunakan modul formatDate untuk memformat tanggal
-    const formattedDeadline = formatDate(deadlineInput.value);
-
+    const deadlineDate = new Date(deadlineInput.value);
+    const formattedDeadline = deadlineDate.toLocaleDateString('id-ID');
+    
     const data = {
         "title": getValue("title"),
         "description": getValue("description"),
         "deadline": formattedDeadline,
+        // "time" : getValue("time"),
     };
 
     postWithToken(target_url, tokenkey, tokenvalue, data, responseData);

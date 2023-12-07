b4843fe392c1652b23fef60b37278649084118f8
dirgafebrian <dirgafebrian45@gmail.com>
2023-12-07 19:13:41 +0700

    Fix redirect URL in redirect.js

--- assets/js/redirect.js
+++ assets/js/redirect.js
@@ -7,7 +7,7 @@ const checkTokenAndRedirect = () => {
 
     if (!tokenValue) {
         // console.log("Token not found, redirecting to login.");
-        window.location.href = "login.html";
+        window.location.href = "./login.html";
     }
 }
 

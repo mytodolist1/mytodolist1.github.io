d4095e473f4a9545256d8bfe7c553a08b21f3c61
dirgafebrian <dirgafebrian45@gmail.com>
2023-12-16 21:41:35 +0700

    update format date time

--- assets/js/complement/timestamp.js
+++ assets/js/complement/timestamp.js
@@ -1,22 +1,11 @@
-export const convertToLocal = (timestamp) => {
-    const date = new Date(timestamp);
-
-    // Format tanggal (yyyy-mm-dd)
-    const year = date.getFullYear();
-    const month = ('0' + (date.getMonth() + 1)).slice(-2);
-    const day = ('0' + date.getDate()).slice(-2);
-    const formattedDate = year + '-' + month + '-' + day;
-  
-    // Format waktu (hh:mm:ss)
-    const hours = ('0' + date.getHours()).slice(-2);
-    const minutes = ('0' + date.getMinutes()).slice(-2);
-    const seconds = ('0' + date.getSeconds()).slice(-2);
-    const formattedTime = hours + ':' + minutes + ':' + seconds;
-  
-    return formattedDate + ' ' + formattedTime;
+export function convertToLocal (timestamp) {
+    var date = new Date(timestamp);
+    var localDate = date.toLocaleDateString();
+    var localTime = date.toLocaleTimeString();
+    return localDate + ' ' + localTime;
 }
 
-export const format12Hours = (time) => {
+export function format12Hours (time) {
     const date = new Date('2000-01-01T' + time + ':00');
     const options = { hour: 'numeric', minute: 'numeric', hour12: true };
     const formattedTime = date.toLocaleString([], options);
@@ -24,7 +13,7 @@ export const format12Hours = (time) => {
     return formattedTime;
 }
 
-export const formatDate = (datestring) => {
+export function formatDate (datestring) {
     const date = new Date(datestring);
     const month = ('0' + (date.getMonth() + 1)).slice(-2);
     const day = ('0' + date.getDate()).slice(-2);

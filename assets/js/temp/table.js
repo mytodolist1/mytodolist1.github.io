d07266cc50dccd7f859d8dffd0e106f1dddddb31
dirgafebrian <dirgafebrian45@gmail.com>
2023-12-17 02:34:51 +0700

    Update button and navbar styles

--- assets/js/temp/table.js
+++ assets/js/temp/table.js
@@ -2,7 +2,7 @@
 export const formTodolist = `
   <td></td>
   <td data-label="Status" class="buttons is-left">
-    <button class="button is-link is-outlined" type="button" onclick="doneTodo('#DONE#')">
+    <button class="btn is-link is-outlined" type="button" onclick="doneTodo('#DONE#')">
       <span class="icon"><i class="mdi mdi-check-bold"></i></span>
     </button>
   </td>
@@ -10,10 +10,10 @@ export const formTodolist = `
   <td data-label="Description">#DESCRIPTION#</td>
   <td data-label="Deadline">#DEADLINE# #TIME#</td>
   <td data-label="Action" class="buttons is-left">
-      <a href="edit.html?_id=#IDEDIT#" class="button is-dark" data-todo-id="#IDHAPUS#" type="button">
+      <a href="edit.html?_id=#IDEDIT#" class="btn is-dark" data-todo-id="#IDHAPUS#" type="button">
         <span class="icon"><i class="mdi mdi-eye-circle"></i></span>
       </a>
-      <button class="button is-dark is-outlined" type="button" onclick="deleteTodo('#DELETE#')">
+      <button class="btn is-dark is-outlined" type="button" onclick="deleteTodo('#DELETE#')">
         <span class="icon"><i class="mdi mdi-delete-circle"></i></span>
       </button>
   </td>
@@ -46,15 +46,21 @@ export const formProfile = `
 `;
 
 export const navbarUser = `
-  <a class="navbar-link is-arrowless">
-  <div class="is-user-avatar">
-    <img src="../assets/img/todolist.ico">
+  <p class="menu-label"></p>
+  <div class="menu-list">
+    <div class="menu-item">
+      <div class="menu-content">
+        <div class="is-user-avatar">
+          <img src="../assets/img/todolist.ico" alt="#USERNAME#">
+        </div>
+        <div class="is-user-info">
+          <div class="is-user-name">
+            <span class="menu-item-label">#USERNAME#</span>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-  <div class="is-user-name">
-    <span id="username">#USERNAME#</span>
-  </div>
-  <span class="icon"><i class="mdi mdi-chevron-down"></i></span>
-  </a>
 `;
 
 

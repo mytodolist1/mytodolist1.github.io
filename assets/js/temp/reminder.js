59641684e38d0a5e47d07c30a37cc2eae38a4fe9
dirgafebrian <dirgafebrian45@gmail.com>
2023-12-17 20:40:33 +0700

    fix reminder.js

--- assets/js/temp/reminder.js
+++ assets/js/temp/reminder.js
@@ -1,49 +1,40 @@
 export function setReminder(date, time) {
-  const currentDate = new Date().getTime();
-  const deadline = new Date(date + " " + time).getTime();
-  const alertReminder = deadline - (10 * 60 * 1000);
-  const alertDifference = alertReminder - currentDate;
+    const currentDate = new Date().getTime();
+    const deadline = new Date(date + " " + time).getTime();
 
-  if (alertDifference > 0) {
-      setTimeout(function() {
-          Swal.fire({
-              icon: "warning",
-              title: "10 menit lagi tugasnya masuk deadline nih, jangan lupa selesaikan atau update ya!",
-              confirmButtonColor: "#3085d6",
-              confirmButtonText: "Baiklah",
+    const alertReminder = deadline - (10 * 60 * 1000)
+    const alertDifference = alertReminder - currentDate;
+
+    const alertDifferences = deadline - currentDate;
+
+    setTimeout(function() {
+        Swal.fire({
+            icon: "info",
+            title: "10 menit lagi tugasnya masuk deadline nih, jangan lupa selesaikan atau update ya!",
+            confirmButtonColor: "#3085d6",
+            confirmButtonText: "Baiklah",
           }).then((result) => {
-              if (result.isConfirmed) {
-                  Swal.fire({
-                      icon: "info",
-                      title: "Silahkan selesaikan atau update tugasnya ya!",
-                      showConfirmButton: false, 
-                  });
-              }
-          });
-      }, alertDifference);
+            if (result.isConfirmed) {
+              Swal.fire({
+                icon: "info",
+                title: "Silahkan selesaikan atau update tugasnya ya!",
+                showConfirmButton: false,
+              });
+            }
+        });
+    }, alertDifference);
 
-  } else if (alertDifference === 0) {
-      setTimeout(function() {
-          Swal.fire({
-              icon: "warning",
-              title: "Sepertinya tugas kamu sudah melewati deadline nih, jangan lupa selesaikan ya!",
-              showConfirmButton: false,
-          });
-      }, alertDifference);
+    setTimeout(function() {
+        Swal.fire({
+            icon: "warning",
+            title: "Sepertinya tugas kamu sudah melewati deadline nih, jangan lupa selesaikan ya!",
+            showConfirmButton: false,
+        });
+    }, alertDifferences);
 
-  } else {
-      setTimeout(function() {
-          Swal.fire({
-              icon: "warning",
-              title: "Udah lewat loh!",
-              showConfirmButton: false,
-          });
-      }, alertDifference);
-  }
-
-  // console.log("Current Date:", currentDate);
-  // console.log("Deadline:", deadline);
-  // console.log("Alert Reminder:", alertReminder);
-  // console.log("Alert Difference:", alertDifference);
-  // console.log("Alert Differences:", alertDifferences);
+    // console.log("Current Date:", currentDate);
+    // console.log("Deadline:", deadline);
+    // console.log("Alert Reminder:", alertReminder);
+    // console.log("Alert Difference:", alertDifference);
+    // console.log("Alert Differences:", alertDifferences);
 }

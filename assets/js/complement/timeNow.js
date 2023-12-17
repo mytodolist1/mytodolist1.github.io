7974d1c0fe7a770bccb2c3939d003e4d421ce2aa
dirgafebrian <dirgafebrian45@gmail.com>
2023-12-17 19:16:00 +0700

    fix date format and update category list

--- assets/js/complement/timeNow.js
+++ assets/js/complement/timeNow.js
@@ -1,14 +1,14 @@
 const showDateTimeNow = () => {
     const date = new Date();
 
+    const month = ('0' + (date.getMonth() + 1)).slice(-2);
+    const day = ('0' + date.getDate()).slice(-2);
+    const year = date.getFullYear();
+
     const hours = ('0' + date.getHours()).slice(-2);
     const minutes = ('0' + date.getMinutes()).slice(-2);
 
-    const year = date.getFullYear();
-    const month = ('0' + (date.getMonth() + 1)).slice(-2);
-    const day = ('0' + date.getDate()).slice(-2);
-
-    const dateNow = month + '/' + day + '/' + year;
+    const dateNow = year + '-' + month + '-' + day;
     const timeNow = hours + ':' + minutes;
 
     return { dateNow, timeNow };

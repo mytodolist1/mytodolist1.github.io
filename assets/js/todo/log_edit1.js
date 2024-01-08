6f6ef6481159cdeb1237f99c6c5f920c5b1c0b4c
dirgafebrian <fdirga63@gmail.com>
2024-01-08 10:44:31 +0700

    Refactor log_edit.js and add log_edit1.js

--- assets/js/todo/log_edit1.js
+++ assets/js/todo/log_edit1.js
@@ -4,6 +4,29 @@ import { getWithToken } from "../temp/component.js";
 
 const target_url = "https://asia-southeast2-mytodolist-402507.cloudfunctions.net/mytodolist-logTodo";
 
+const dataTodoOld  = (value) => {
+    console.log("value: ", value);
+
+    dataold = value.change[0].dataold;
+    const data = formTodolist1
+    .replace("#TITLE#", dataold.title)
+    .replace("#DESCRIPTION#", dataold.description)
+    .replace("#DEADLINE#", dataold.deadline)
+    .replace("#TIME#", dataold.time)
+    .replace("#CATEGORY#", dataold.tags.category)
+
+    addInner("tableTodolistOld", data);
+}
+
+const responseDataOld = (result) => {
+    console.log("result: ", result);
+    if (result.status === true) {
+        result.data.forEach(dataTodoOld);
+    }
+}
+
+getWithToken(target_url, responseDataOld);
+
 const dataTodoNew  = (value) => {
     console.log("value: ", value);
 

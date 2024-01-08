a73a40f3c6520d146863f034c1b33ed6ef83b971
dirgafebrian <dirgafebrian45@gmail.com>
2024-01-08 10:49:37 +0700

    Refactor getWithToken function in log_edit1.js

--- assets/js/todo/log_edit1.js
+++ assets/js/todo/log_edit1.js
@@ -26,28 +26,3 @@ const responseDataOld = (result) => {
 }
 
 getWithToken(target_url, responseDataOld);
-
-const dataTodoNew  = (value) => {
-    console.log("value: ", value);
-
-    latest = value.change[value.change.length - 1];
-    datanew = latest.datanew;
-
-    const data = formTodolist1
-    .replace("#TITLE#", datanew.title)
-    .replace("#DESCRIPTION#", datanew.description)
-    .replace("#DEADLINE#", datanew.deadline)
-    .replace("#TIME#", datanew.time)
-    .replace("#CATEGORY#", datanew.tags.category)
-
-    addInner("tableTodolistNew", data);
-}
-
-const responseDataNew = (result) => {
-    console.log("result: ", result);
-    if (result.status === true) {
-        result.data.forEach(dataTodoNew);
-    }
-}
-
-getWithToken(target_url, responseDataNew);

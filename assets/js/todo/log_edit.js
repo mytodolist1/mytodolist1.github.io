f28989bca0aeb83c63ee9379b086ebd4486503e6
dirgafebrian <fdirga63@gmail.com>
2024-01-08 10:37:18 +0700

    Add navigation buttons to list_kegiatan.html and create log_edit.js for logging todo edits

---
+++ assets/js/todo/log_edit.js
@@ -0,0 +1,53 @@
+import { addInner } from "https://jscroot.github.io/element/croot.js";
+import { formTodolist } from "../temp/table.js";
+import { getWithToken } from "../temp/component.js";
+
+const target_url = "https://asia-southeast2-mytodolist-402507.cloudfunctions.net/mytodolist-logTodo";
+
+const dataTodoOld  = (value) => {
+    console.log("value: ", value);
+
+    dataold = value.change[0].dataold;
+    const data = formTodolist
+    .replace("#TITLE#", dataold.title)
+    .replace("#DESCRIPTION#", dataold.description)
+    .replace("#DEADLINE#", dataold.deadline)
+    .replace("#TIME#", dataold.time)
+    .replace("#CATEGORY#", dataold.tags.category)
+
+    addInner("tableTodolistOld", data);
+}
+
+const responseDataOld = (result) => {
+    console.log("result: ", result);
+    if (result.status === true) {
+        result.data.forEach(dataTodoOld);
+    }
+}
+
+getWithToken(target_url, responseDataOld);
+
+const dataTodoNew  = (value) => {
+    console.log("value: ", value);
+
+    latest = value.change[value.change.length - 1];
+    datanew = latest.datanew;
+
+    const data = formTodolist
+    .replace("#TITLE#", datanew.title)
+    .replace("#DESCRIPTION#", datanew.description)
+    .replace("#DEADLINE#", datanew.deadline)
+    .replace("#TIME#", datanew.time)
+    .replace("#CATEGORY#", datanew.tags.category)
+
+    addInner("tableTodolistNew", data);
+}
+
+const responseDataNew = (result) => {
+    console.log("result: ", result);
+    if (result.status === true) {
+        result.data.forEach(dataTodoNew);
+    }
+}
+
+getWithToken(target_url, responseDataNew);

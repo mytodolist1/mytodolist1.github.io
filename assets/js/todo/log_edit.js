ce6d7b8d4eee8d34fc6f5556cc739b3f2c009651
dirgafebrian <dirgafebrian45@gmail.com>
2024-01-08 11:26:32 +0700

    Refactor table.js and log_edit.js

--- assets/js/todo/log_edit.js
+++ assets/js/todo/log_edit.js
@@ -1,8 +1,34 @@
 import { addInner } from "https://jscroot.github.io/element/croot.js";
-import { formTodolist1 } from "../temp/table.js";
+import { formTodolist } from "../temp/table.js";
 import { getWithToken } from "../temp/component.js";
 
 const target_url = "https://asia-southeast2-mytodolist-402507.cloudfunctions.net/mytodolist-logTodo";
+
+const dataTodoOld  = (value) => {
+    console.log("value: ", value);
+
+    const dataold = value.change[0].dataold;
+
+    console.log("dataold: ", dataold);
+
+    const data = formTodolist
+    .replace("#TITLE#", dataold.title)
+    .replace("#DESCRIPTION#", dataold.description)
+    .replace("#DEADLINE#", dataold.deadline)
+    .replace("#TIME#", dataold.time)
+    .replace("#CATEGORY#", dataold.tags.category)
+
+    addInner("tableTodolistOld", data);
+}
+
+const responseDataOld = (result) => {
+    console.log("result: ", result);
+    if (result.status === true) {
+        result.data.forEach(dataTodoOld);
+    }
+}
+
+getWithToken(target_url, responseDataOld);
 
 const dataTodoNew  = (value) => {
     console.log("value: ", value);
@@ -10,7 +36,7 @@ const dataTodoNew  = (value) => {
     const latest = value.change[value.change.length - 1];
     const datanew = latest.datanew;
 
-    const data = formTodolist1
+    const data = formTodolist
     .replace("#TITLE#", datanew.title)
     .replace("#DESCRIPTION#", datanew.description)
     .replace("#DEADLINE#", datanew.deadline)
